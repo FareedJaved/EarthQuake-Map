6bc1d4857c023f5807a58fd8710342ed26fd68a4
Fareed Javed <fareedjaved@Fareeds-MBP.home>
2016-03-15 17:37:22 -0400

    Implemented CityMarker

--- UCSDUnfoldingMaps/src/module4/CityMarker.java
+++ UCSDUnfoldingMaps/src/module4/CityMarker.java
@@ -9,7 +9,7 @@ import processing.core.PGraphics;
 /** Implements a visual marker for cities on an earthquake map
  * 
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * @author Fareed Javed
  *
  */
 public class CityMarker extends SimplePointMarker {
@@ -43,7 +43,10 @@ public class CityMarker extends SimplePointMarker {
 		pg.pushStyle();
 		
 		// TODO: Add code to draw a triangle to represent the CityMarker
-		
+		// Adding code to draw a triangle to represent the CityMarker
+		pg.fill(146, 9, 9);
+		pg.triangle(x, y - TRI_SIZE, x - TRI_SIZE, y + TRI_SIZE, x + TRI_SIZE, y + TRI_SIZE);
+				
 		// Restore previous drawing style
 		pg.popStyle();
 	}
